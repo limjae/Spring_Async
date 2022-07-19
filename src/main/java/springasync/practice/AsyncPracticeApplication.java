cb97da5c4e74ca67125463068125755b71139dd8
limjae <limjae96@gmail.com>
2022-07-19 14:50:06 +0900

    Initial commit

---
+++ src/main/java/springasync/practice/AsyncPracticeApplication.java
@@ -0,0 +1,13 @@
+package springasync.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsyncPracticeApplication.class, args);
+    }
+
+}
